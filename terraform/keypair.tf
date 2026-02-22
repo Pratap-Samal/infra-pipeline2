6f8b59d1dd34dc1f76aece3ef95a1116a74e1573
Pratap-Samal <mail2pratapat@gmail.com>
2026-02-22 10:38:51 +0530

    Fourth commit

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,5 +1,7 @@
 
 resource "aws_key_pair" "deployer" {
   key_name   = "pratap-key"
-  public_key = file("/home/ec2-user/.ssh/infra-pipeline2/pratap-key.pub")
+  public_key = file("${path.module}/pratap-key.pub")
+
+  
 }
