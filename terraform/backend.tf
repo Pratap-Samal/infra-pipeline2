c9d4668f4f590274a909130157a3fa17b4b8daa5
Pratap-Samal <mail2pratapat@gmail.com>
2026-03-11 21:46:03 +0530

    22-one more commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "pratap-terraform-state-bucket"
-    key            = "infra-pipeline/terraform.tfstate"
+    key            = "infra-pipeline2/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,7 +1,5 @@
 
 resource "aws_key_pair" "deployer" {
   key_name   = "pratap-key"
-  public_key = file("${path.module}/pratap-key.pub")
-
-  
+  public_key = file("${path.module}/pratap-key.pub") 
 }
